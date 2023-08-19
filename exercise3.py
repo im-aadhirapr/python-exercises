d1e31a76fb51fe194b57943449b8d7d915ca9153
Aathira P R <aadhirapr@gmail.com>
2023-08-20 00:26:41 +0530

    Added exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,15 @@
+# Program to add digits of a number
+
+number = input("Enter the number ")
+num1 = number[0]
+num2 = number[1]
+sum = int(num1) + int(num2)
+print(sum)
+
+# To calculate the BMI
+
+weight = int(input("Enter the weight"))
+height = int(input("Enter the height"))
+calculate_bmi = weight / (height * height)
+print("The body mass index of the person is " +str(calculate_bmi))
+
