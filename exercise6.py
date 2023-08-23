eedb9a6c0b2321288c3d744e3699fb9c018e08c8
Aathira P R <aadhirapr@gmail.com>
2023-08-23 23:58:07 +0530

    Added new file exercise6 doing with the nested loop concept

---
+++ exercise6.py
@@ -0,0 +1,9 @@
+# nested list problem
+
+row1 = [1,1,1]
+row2 = [1,1,1]
+row3 = [1,1,1]
+matrix = (f"{row1}\n{row2}\n{row3}")
+print(matrix)
+
+cross_mark = input("Enter the matrix position to which x to be inserted")
